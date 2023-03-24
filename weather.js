19cf7e979c1fb72efa66ac2bbc1014fd7c373c19
Paul Lazar <paullazar@MASTUDENT-Paul-Lazar-C17GMAXPQ6L4.local>
2023-03-24 11:32:15 +0000

    Printed main and
    a
    q

--- weather.js
+++ weather.js
@@ -1,3 +1,16 @@
 const apiKey = require ('./apiKey.js');
+const city = 'London';
+const apiUrl = `http://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`;
 
-const apiUrl = `http://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}
+
+let weatherData = null;
+
+fetch(apiUrl)
+  .then((response) => response.json())
+  .then((weatherData) => {
+    //console.log(weatherData)
+    console.log(weatherData.main)
+    console.log(weatherData.weather[0].main)
+  });
+
+console.log('Requesting weather data');
